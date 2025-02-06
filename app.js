3f1cdf6b667e13048657c89c2eb0cd27c988de95
Bruno DAlmeida <bdalmeida@gmail.com>
2025-02-05 19:27:46 -0700

    Primeiro commit do projeto AmigoSecreto

--- app.js
+++ app.js
@@ -1,21 +1,61 @@
-// 
 let listaParticipantes = [];
+let NomeCompleto= "";
 
 
+function adicionarAmigo (){
+// Verifica se o nome a inserir é valido, sem caracteres especiais, numeros ou vazio, adicionando a lista de participantes.
+// Exibe na tela o nome inserido e limpa o campo para o proximo nome a inserir.
 
-function adicionarAmigo (NomeCompleto){
-// Verifica se o nome a inserir é valido, sem caracteres especiais e numeros, adicionando a lista de participantes
-        if (apenasLetras (NomeCompleto)) then 
-            let NomeCompleto = document.querySelector('amigo').value;
-            listaParticipantes.push(NomeCompleto);
+    NomeCompleto = document.getElementById("amigo").value;
+
+    if (NomeCompleto.trim() === "")
+        alert (`Por favor, insira um nome.`);
+    else {   
+        if (apenasLetras (NomeCompleto)) {
+            
+            if (nomeRepetido(NomeCompleto)) 
+                alert (`Nome já inserido. Por favor insira um novo nome.`);
+            else {            
+                listaParticipantes.push(NomeCompleto);
+                atualizarLista();
+            }
+        }
         else 
-            alert (`Nome inválido!`);
+            alert (`Por favor insira um nome válido.`);
+    }
+
+    document.getElementById("amigo").value = ""; 
 }
 
+function nomeRepetido (nome){
+    return listaParticipantes.includes(nome);
+}
 
 function apenasLetras(texto) {
-// Retorna TRUE se o nome é valido (texto somente) ou FALSE se tem caracteres alem de texto e espaco
-    if (texto.trim() === "") return false;
     return /^[A-Za-zÀ-ÖØ-öø-ÿ\s]+$/.test(texto);
 }
 
+function atualizarLista() {
+    let listaAmigos = document.getElementById("listaAmigos");
+    listaAmigos.innerHTML = ""; 
+    listaParticipantes.forEach((nome) => {
+        let li = document.createElement("li");
+        li.textContent = nome;
+        listaAmigos.appendChild(li);
+    });
+}
+
+
+function sortearAmigo() {
+    
+    document.getElementById("amigo").value = ""; 
+
+    if (listaParticipantes.length == 0 )
+        alert (`Adicione os amigos a serem sorteados antes.`);
+    else {
+        let sorteado = parseInt(Math.random () * listaParticipantes.length + +1);
+        document.getElementById("amigo").value = "Seu amigo secreto é " + listaParticipantes[sorteado -1]; 
+        listaAmigos.innerHTML = "";
+    }
+    
+}
